4a9188270fe44869d8e3b6a97f5af92ee93829af
Tim2017dn <dn.tim2017@gmail.com>
2020-04-07 09:12:07 +0800

    test jenkins

--- chemicals_back/src/test/java/com/nju/chemicals/ChemicalsApplicationTests.java
+++ chemicals_back/src/test/java/com/nju/chemicals/ChemicalsApplicationTests.java
@@ -1,14 +1,19 @@
 package com.nju.chemicals;
 
+import com.nju.chemicals.controller.HelloController;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = HelloController.class)
 @SpringBootTest
-class ChemicalsApplicationTests {
+public class ChemicalsApplicationTests {
 
-
+    @Autowired
+    HelloController hc;
 
     //记录器
     Logger logger = LoggerFactory.getLogger(getClass());
@@ -23,6 +28,12 @@ class ChemicalsApplicationTests {
         logger.info("这是info日志...");
         logger.warn("这是warn日志...");
         logger.error("这是error日志...");
+    }
+
+    @Test
+    public void sayHello(){
+
+        System.out.println(hc.sayHello());
     }
 
 }
